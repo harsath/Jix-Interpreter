8012cbfd22b25e34f581c992287510cdfbe65c77
harsath <harsath@mm.st>
2024-06-02 19:36:27 -0400

    modify builtin fn parameter handling

--- includes/builtin_functions.h
+++ includes/builtin_functions.h
@@ -1,12 +1,12 @@
 #ifndef BUILTIN_FUNCTIONS_H
 #define BUILTIN_FUNCTIONS_H
 
-#include <stdio.h>
 #include "hash_table.h"
 #include "interpreter.h"
+#include <stdio.h>
 #define NUM_BUILTIN_FNS 1
 
-void builtin_print(struct object*);
+void *builtin_print(struct object *);
 
 struct builtin_fn {
   void *fn_ptr;
@@ -14,6 +14,7 @@ struct builtin_fn {
 };
 
 struct hash_table *init_and_register_builtin_fns();
-struct builtin_fn *lookup_builtin_fns(struct hash_table *table, const char *key);
+struct builtin_fn *lookup_builtin_fns(struct hash_table *table,
+                                      const char *key);
 
 #endif
--- src/builtin_functions.c
+++ src/builtin_functions.c
@@ -7,18 +7,20 @@ struct hash_table *init_and_register_builtin_fns() {
     return NULL;
   }
 
-  struct builtin_fn *builtin_fns = malloc(sizeof(struct builtin_fn) * NUM_BUILTIN_FNS);
+  struct builtin_fn *builtin_fns =
+      malloc(sizeof(struct builtin_fn) * NUM_BUILTIN_FNS);
   builtin_fns[0].fn_ptr = builtin_print;
   builtin_fns[0].num_parameters = 1;
   hash_table_insert(fn_table, strdup("print"), builtin_fns);
   return fn_table;
 }
 
-struct builtin_fn *lookup_builtin_fns(struct hash_table *table, const char *key) {
+struct builtin_fn *lookup_builtin_fns(struct hash_table *table,
+                                      const char *key) {
   return hash_table_lookup(table, key);
 }
 
-void builtin_print(struct object *value) {
+void *builtin_print(struct object *value) {
   switch (value->data_type) {
   case INT_VALUE:
     printf("%li\n", value->int_value);
@@ -33,4 +35,5 @@ void builtin_print(struct object *value) {
     printf("Invalid datatype in print() function.");
     exit(1);
   }
+  return NULL;
 }
