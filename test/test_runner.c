4d55eb54b6ed73783edd68c12dbeff64c28720aa
harsath <harsath@mm.st>
2024-06-02 13:16:13 -0400

    more tests

--- test/test_runner.c
+++ test/test_runner.c
@@ -5,24 +5,39 @@ int main(int argc, const char *argv[]) {
 
 	const char *test_files[] = {
 		"var_decl.jix",
-		"var_assign.jix"
+		"var_assign.jix",
+		"if_else_stmt.jix",
+		"while_stmt.jix",
+		"for_stmt.jix",
+		"break_stmt.jix",
+		"functions.jix",
 	};
 
 	long expected_results[] = {
 		10,
-		40
+		40,
+		50,
+		10,
+		10,
+		20,
+		10,
 	};
 
 	const char *test_name[] = {
 		"Variable declaration",
-		"Variable assignment"
+		"Variable assignment",
+		"If-Else statement",
+		"While statement",
+		"For statement",
+		"Break statement",
+		"Functions",
 	};
 
 	size_t total_tests = sizeof(test_files) / sizeof(test_files[0]);
 
 	for (size_t i = 0; i < total_tests; i++) {
 		struct object *return_value = interpreter_pipeline(test_files[i]);
-		JIX_ASSERT_TRUE(return_value->int_value == expected_results[i], test_name[i]);
+		JIX_ASSERT_TRUE(expected_results[i], return_value->int_value, test_name[i]);
 	}
 
 	JIX_TEST_STATS();
